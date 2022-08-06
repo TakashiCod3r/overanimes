3004f61ca66553b845e9065fc1cb1a171c8e810b
TakashiCod3r <luizhenriquecanazart@gmail.com>
2022-08-05 21:53:48 -0300

    tela login

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('.input');
+
+const handleFocus = ({ target }) => {
+    const span = target.previousElementSibling;
+    span.classList.add('span-active'); /* clica no input ele sobe*/
+  }
+
+  const handleFocusOut = ( {target}) => {
+if(target.value == '') { /* verifica se tem algo dentro do input, se tiver ele não voltara
+ao normal*/
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');  /* quando clica em outro input, o outro input ja clicado 
+    anteriormente volta ao normal*/
+}
+
+  }
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus)); 
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut)); 
