dfff89c9597b8db02db16260437681012591798b
TakashiCod3r <luizhenriquecanazart@gmail.com>
2022-08-06 01:19:01 -0300

    1

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const handleFocus = ({ target }) => {
     span.classList.add('span-active'); /* clica no input ele sobe*/
   }
 
+
   const handleFocusOut = ( {target}) => {
 if(target.value == '') { /* verifica se tem algo dentro do input, se tiver ele não voltara
 ao normal*/
@@ -12,8 +13,13 @@ ao normal*/
     span.classList.remove('span-active');  /* quando clica em outro input, o outro input ja clicado 
     anteriormente volta ao normal*/
 }
+  }
 
+  const handleChange = () =>{
+
+    const [username, password] = inputs;
   }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus)); 
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut)); 
+inputs.forEach((input) => input.addEventListener('input', handleChange)); 
